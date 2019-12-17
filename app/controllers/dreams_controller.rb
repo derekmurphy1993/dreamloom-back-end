17219d5d473f635683543c00b060749e9ac71c43
Derek <robinmurphy@Robins-MacBook-Pro.local>
2019-12-17 08:55:21 -0500

    Deploy
    New dream controller

--- app/controllers/dreams_controller.rb
+++ app/controllers/dreams_controller.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
-class DreamsController < OpenReadController #change to PROTECTED
+class DreamsController < ProtectedController
   before_action :set_dream, only: %i[show update destroy]
 
   # GET /dreams
   def index
-    @dreams = Dream.all
+    @dreams = current_user.dreams.all
 
     render json: @dreams
   end
@@ -17,7 +17,7 @@ class DreamsController < OpenReadController #change to PROTECTED
 
   # POST /dreams
   def create
-    @dream = Dream.new(dream_params)
+    @dream = current_user.dreams.build(dream_params)
 
     if @dream.save
       render json: @dream, status: :created, location: @dream
@@ -44,7 +44,7 @@ class DreamsController < OpenReadController #change to PROTECTED
 
     # Use callbacks to share common setup or constraints between actions.
     def set_dream
-      @dream = Dream.find(params[:id])
+      @dream = current_user.dreams.find(params[:id])
     end
 
     # Only allow a trusted parameter "white list" through.
